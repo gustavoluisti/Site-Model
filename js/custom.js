1b32b0a3e8a69f0bfbd5d71becf5b2d5294cf87f
Luis Gustavo Santos <spartatecnologia@gmail.com>
2018-03-03 22:48:26 -0300

    Atualizando

--- js/custom.js
+++ js/custom.js
@@ -6,3 +6,30 @@ $(function() {
     // animando on scroll
     new WOW().init();
 });
+
+/*========================================================
+                    WORK SPARTAN
+==========================================================*/
+
+$(function() {
+
+    $("#work").magnificPopup({
+        delegate: 'a',
+        type: 'image',
+        gallery:{
+            enabled:true
+        }
+    });
+});
+/*========================================================
+                    TEAM SPARTAN
+==========================================================*/
+$(document).ready(function(){
+    $("#team-members").owlCarousel({
+        items: 3,
+        autoplay: true,
+        smartSpeed: 700,
+        loop: true,
+        autoplayHoverPause: true
+    });
+  });
